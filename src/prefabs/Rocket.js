c85b816debf2f4a1ea14232c9d87e9ffccc38dbb
vekim00100 <vekim@ucsc.edu>
2024-01-24 14:44:54 -0800

    Before Explosions in The Sky

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,34 @@
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame)
+
+        scene.add.existing(this)    // add objects to existing scene
+        this.isFiring = false       // track rocket's firing status
+        this.moveSpeed = 2          // rocket speed in pixels/frame
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.with) {
+                this.x -= this.moveSpeed
+            } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed
+            }
+        }
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true
+        }
+        // if fired. move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed
+        }
+        // reset on miss 
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.isFiring = false
+            this.y = game.config.height - borderUISize - borderPadding
+        }
+    }
+}
