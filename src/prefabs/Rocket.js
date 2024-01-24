bf03baa7528fd6ee1303889baa5d80bf07263165
vekim00100 <vekim@ucsc.edu>
2024-01-24 14:45:54 -0800

    Before Explosions in The Sky

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -31,4 +31,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.y = game.config.height - borderUISize - borderPadding
         }
     }
+
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false
+        this.y = game.config.height - borderUISize - borderPadding
+    }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -18,7 +18,12 @@ class Play extends Phaser.Scene {
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0)
-    
+
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0)
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0)
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0)
+        
         // define keys
         keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
         keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
@@ -28,7 +33,37 @@ class Play extends Phaser.Scene {
 
     update() {
         this.starfield.tilePositionX -= 4
+        
         this.p1Rocket.update
+
+        this.ship01.update()        // update spaceships (x3)
+        this.ship02.update()
+        this.ship03.update()
+
+        // check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship03)) {
+            this.p1Rocket.reset()
+            this.ship03.reset()
+        }
+        if(this.checkCollision(this.p1Rocket, this.ship02)) {
+            this.p1Rocket.reset()
+            this.ship02.reset()
+        }
+        if(this.checkCollision(this.p1Rocket, this.ship01)) {
+            this.p1Rocket.reset()
+            this.ship01.reset()
+        }
     }
 
+    checkCollision(rocket, ship) {
+        // simple AABB checking
+        if (rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x && 
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship. y) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
